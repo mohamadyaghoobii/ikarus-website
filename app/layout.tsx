eae349b869c60b1a727807e3a8f868aa363937f5
mehrad <mohamadyaghoobi1997@gmail.com>
2024-10-04 18:01:11 +0330

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Ikarus web developing ",
+  description: "Devloping modern applications and websites with web3 and ai technology",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/IkarusLogo.jpg" sizes="any" />
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ data/index.ts
@@ -0,0 +1,238 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I simulate cyber attack against your website to check for exploitable vulnerabilities. ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/securiy.jpg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm Capable  of  speaking English,French,German,Italian to cooperate with foreign companies",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently working on a smart contract project with solidity coding",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.gif",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "www.l8-cafe.com",
+    des: "A Fullstack website with sign up and dynamic menu",
+    img: "/Cafe.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg"],
+    link: "https://l8-cafe.com",
+  },
+  {
+    id: 2,
+    title: "smart contract",
+    des: "A Fullstack website in field of web 3 security and Ai ",
+    img: "/ikarusweb.jpg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+    link: "https://ikarusbiz.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Aigram",
+    des: "A react native application to upload your videos",
+    img: "/aigram.jpg",
+    iconLists: ["/re.svg", "/expo.svg", "/ts.svg"],
+  },
+  {
+    id: 4,
+    title: "identifier web3",
+    des: "A web3 project that create an identity for you",
+    img: "/identifier.png",
+    iconLists: ["/solidity.svg", "/next.svg", "/ts.svg"],
+    link: "https://identifierforyou.vercel.app/",
+  },
+  {
+    id: 5,
+    title: "chat-gpt",
+    des: "A bot that use open ai to answering your questions",
+    img: "/ikaruschat-gpt.png",
+    iconLists: ["/re.svg", "/nodejs.svg", "/ts.svg"],
+  },
+  {
+    id: 6,
+    title: "A professional online shop",
+    des: "A react website with node js as backend",
+    img: "/zamzamshop.png",
+    iconLists: ["/re.svg", "/nodejs.svg", "/ts.svg"],
+    link: "https://www.abzamzam.com/",
+  },
+  {
+    id: 7,
+    title: "A nest online shop",
+    des: "A template with nest coding",
+    img: "/nest.png",
+    iconLists: ["/nestjs.svg", "/re.svg", "/ts.svg"],
+  },
+  {
+    id: 8,
+    title: "A web3 news website",
+    des: "A website to realize what s happening in the world of web3",
+    img: "/ikarusmeta.png",
+    iconLists: ["/next.svg", "/ts.svg"],
+    link: "https://ikarusmeta.vercel.app/",
+  }, {
+    id: 9,
+    title: "Tehrangram",
+    des: "A social website for uploading about your daily event",
+    img: "tehrangram.png",
+    iconLists: ["/next.svg", "/ts.svg","re.svg"],
+    link: "https://tehrangram.vercel.app/",
+  },
+  {
+    id: 10,
+    title: "Buzz",
+    des: "A threads application with live chat",
+    img: "buzz (2).png",
+    iconLists: ["re.svg","/nodejs.svg"],
+  },
+    {
+    id: 11,
+    title: "Artosium-Ai.",
+    des: " a Software-as-a-Service app with AI features",
+    img: "artosiumai.png",
+    iconLists: ["/next.svg", "/ts.svg","re.svg"],
+      link: "https://artosium-ai.vercel.app",
+  },
+    {
+    id: 12,
+    title: "Instagram-Clone",
+    des: " A full-stack Instagram clone built with the MERN stack",
+    img: "222.png",
+    iconLists: ["re.svg","/nodejs.svg"],
+     
+  },
+];
+
+
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev ",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/instagram.svg",
+    url: "https://www.instagram.com/ikarus_web",
+  },
+
+];
